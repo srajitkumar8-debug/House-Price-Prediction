5f0ac314543c9169fa39debc0429d59ec0ba0881
srajitkumar8-debug <srajitkumar8@gmail.com>
2025-12-29 10:51:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,4 +45,5 @@ with st.spinner('Predicting House Price'):
   time.sleep(2)
 
 st.success(f'Estimated House Price is: $ {final_price}')
-st.markdown('''*Design and Developed by: Ankit Mishra*''')
+st.markdown('''*Design and Developed by: Srajit Kumar*''')
+
